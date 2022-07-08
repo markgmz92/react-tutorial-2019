b64e9555d89b8634b551e6a91a5ffc9898b174dd
markgmz92 <markgmz92@gmail.com>
2022-07-08 17:01:35 +0800

    [RT - 001] Git push

---
+++ src/components/FeedbackList.jsx
@@ -0,0 +1,39 @@
+//import { motion, AnimatePresence } from 'framer-motion';
+import FeedbackItem from './FeedbackItem';
+
+function FeedbackList({ feedback, handleDelete }) {
+  if (!feedback || feedback.length === 0) {
+    return <p>No Feedback Yet</p>;
+  }
+
+  // return (
+  //   <div className='feedback-list'>
+  //     <AnimatePresence>
+  //       {feedback.map((item) => (
+  //         <motion.div
+  //           key={item.id}
+  //           initial={{ opacity: 0 }}
+  //           animate={{ opacity: 1 }}
+  //           exist={{ opacity: 0 }}
+  //         >
+  //           <FeedbackItem
+  //             key={item.id}
+  //             item={item}
+  //             handleDelete={handleDelete}
+  //           />
+  //         </motion.div>
+  //       ))}
+  //     </AnimatePresence>
+  //   </div>
+  // );
+
+  return (
+    <div>
+      {feedback.map((item) => (
+        <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
+      ))}
+    </div>
+  );
+}
+
+export default FeedbackList;
